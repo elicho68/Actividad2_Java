e2a0033efb41b34098f4fcccc15ba884e7dfea25
elicho68 <elicho68@gmail.com>
2024-09-01 10:32:20 -0600

    primer_commit

---
+++ db_empresa_010924.sql
@@ -0,0 +1,174 @@
+CREATE DATABASE  IF NOT EXISTS `db_empresa` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_empresa`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_empresa
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id_cliente` int NOT NULL AUTO_INCREMENT,
+  `nit` varchar(9) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `apellido` varchar(60) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `telefono` varchar(8) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'38584166','ELI','OCHOA','CIUDAD','49778702','1986-11-08'),(8,'24578','alex','juerez','ciudad','4578774','1986-01-02'),(9,'24578','Alexander','Gomez','ciudad','4578774','1986-01-02'),(10,'24578-9','SAUL','AYALA','SAN MIGUEL','12457457','1990-12-05'),(13,'1234789-4','AXEL','GOMEZ','PETAPA','45784578','1986-12-05'),(15,'CF','DARIO','GOMEZ','VILLA NUEVA','12345678','1986-07-25'),(16,'CF','FELIX','GUZMAN','SAN MIGUEL','45678945','1980-05-25');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleados` (
+  `id_empleados` int NOT NULL AUTO_INCREMENT,
+  `codigo` varchar(20) DEFAULT NULL,
+  `nombres` varchar(60) DEFAULT NULL,
+  `apellidos` varchar(60) DEFAULT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  `telefono` varchar(8) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `id_puesto` smallint DEFAULT NULL,
+  PRIMARY KEY (`id_empleados`),
+  KEY `id_puesto_puesto_empleado_idx` (`id_puesto`),
+  CONSTRAINT `id_puesto_puesto_empleado` FOREIGN KEY (`id_puesto`) REFERENCES `puestos` (`id_puesto`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empleados`
+--
+
+LOCK TABLES `empleados` WRITE;
+/*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
+INSERT INTO `empleados` VALUES (1,'E001','PEDRO','PAZ MAS','GUATEMALA','5555','1986-01-02',1);
+/*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `marcas`
+--
+
+DROP TABLE IF EXISTS `marcas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `marcas` (
+  `id_marcas` smallint NOT NULL AUTO_INCREMENT,
+  `marca` varchar(40) NOT NULL,
+  PRIMARY KEY (`id_marcas`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marcas`
+--
+
+LOCK TABLES `marcas` WRITE;
+/*!40000 ALTER TABLE `marcas` DISABLE KEYS */;
+INSERT INTO `marcas` VALUES (1,'SAMSUNG'),(2,'SONY');
+/*!40000 ALTER TABLE `marcas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos` (
+  `id_productos` int NOT NULL AUTO_INCREMENT,
+  `codigo` varchar(10) DEFAULT NULL,
+  `producto` varchar(50) DEFAULT NULL,
+  `precio_costo` decimal(6,2) DEFAULT NULL,
+  `id_marcas` smallint DEFAULT NULL,
+  PRIMARY KEY (`id_productos`),
+  KEY `id_marcas_marcas_productos_idx` (`id_marcas`),
+  CONSTRAINT `id_marcas_marcas_productos` FOREIGN KEY (`id_marcas`) REFERENCES `marcas` (`id_marcas`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `puestos`
+--
+
+DROP TABLE IF EXISTS `puestos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `puestos` (
+  `id_puesto` smallint NOT NULL AUTO_INCREMENT,
+  `puesto` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_puesto`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `puestos`
+--
+
+LOCK TABLES `puestos` WRITE;
+/*!40000 ALTER TABLE `puestos` DISABLE KEYS */;
+INSERT INTO `puestos` VALUES (1,'Programacion'),(2,'Digitador'),(3,'Asistente'),(4,'Jefe');
+/*!40000 ALTER TABLE `puestos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'db_empresa'
+--
+
+--
+-- Dumping routines for database 'db_empresa'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-01 10:19:54
---
+++ src/modelo/Cliente.java
@@ -0,0 +1,185 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.table.DefaultTableModel;
+import java.sql.ResultSet;
+
+
+/**
+ *
+ * @author Elicho
+ */
+public class Cliente extends Persona {
+private String nit;
+private int id;
+Conexion cn;
+
+    public Cliente(){}
+    public Cliente(int id, String nit, String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento) {
+        super(nombres, apellidos, direccion, telefono, fecha_nacimiento);
+        this.id = id;
+        this.nit = nit;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+@Override
+    public void crear(){
+        try {
+            PreparedStatement parametro;
+            cn = new Conexion();
+            
+            cn.abrir_conexion( );
+            String query ="INSERT INTO clientes (nit,nombre,apellido,direccion,telefono,fecha_nacimiento) VALUES (?,?,?,?,?,?)";
+            parametro = (PreparedStatement) cn.ConexionBD.prepareStatement(query);
+            parametro.setString(1, getNit());
+            parametro.setString(2, getNombres());
+            parametro.setString(3, getApellidos());
+            parametro.setString(4, getDireccion());
+            parametro.setString(5, getTelefono());
+            parametro.setString(6, getFecha_nacimiento());
+            int executar = parametro.executeUpdate();
+            System.out.println("Ingreso Exitoso... " + Integer.toString(executar));
+            
+            cn.cerrar_conexion( );
+            
+        
+        }catch(SQLException ex){
+                System.out.println("Error al crear" + ex.getMessage());
+        }
+    }
+    
+@Override
+    public DefaultTableModel leer(){
+    DefaultTableModel tabla = new DefaultTableModel();
+    try{
+        cn = new Conexion();
+        cn.abrir_conexion();
+        
+        String query ="SELECT * FROM db_empresa.clientes;";
+        ResultSet consulta= cn.ConexionBD.createStatement().executeQuery(query);
+        
+        String encabezado[] = {"id","Nit","Nommbres","Apellidos","Direccion","Telefono","Nacimiento"};
+        tabla.setColumnIdentifiers(encabezado);
+        String datos[]= new String[7];
+        while (consulta.next()){
+        datos[0]=consulta.getString("id_cliente");
+        datos[1]=consulta.getString("nit");
+        datos[2]=consulta.getString("nombre");
+        datos[3]=consulta.getString("apellido");
+        datos[4]=consulta.getString("direccion");
+        datos[5]=consulta.getString("telefono");
+        datos[6]=consulta.getString("fecha_nacimiento");
+        
+        tabla.addRow(datos);
+        
+        }
+        
+        cn.cerrar_conexion();
+    
+    }catch (SQLException ex){
+        cn.cerrar_conexion();
+                   System.out.println("Error al leer" + ex.getMessage());
+    }
+    
+    return tabla;
+    
+    
+    }
+
+@Override
+public void actualizar(){
+        try {
+            PreparedStatement parametro;
+            cn = new Conexion();
+            
+            cn.abrir_conexion( );
+            String query ="UPDATE clientes SET nit = ?, nombre = ?, apellido = ?, direccion = ?,telefono = ?, fecha_nacimiento = ? WHERE id_cliente = ?;";
+            parametro = (PreparedStatement) cn.ConexionBD.prepareStatement(query);
+            parametro.setString(1, getNit());
+            parametro.setString(2, getNombres());
+            parametro.setString(3, getApellidos());
+            parametro.setString(4, getDireccion());
+            parametro.setString(5, getTelefono());
+            parametro.setString(6, getFecha_nacimiento());
+            parametro.setInt(7, getId());            
+            int executar = parametro.executeUpdate();
+            System.out.println("Actualizacion Exitoso... " + Integer.toString(executar));
+            
+            cn.cerrar_conexion( );
+            
+        
+        }catch(SQLException ex){
+                System.out.println("Error al Actualizar" + ex.getMessage());
+        }
+    
+}
+
+@Override
+public void borrar(){
+try {
+            PreparedStatement parametro;
+            cn = new Conexion();
+            
+            cn.abrir_conexion( );
+            String query ="DELETE FROM clientes WHERE id_cliente = ?;";
+            parametro = (PreparedStatement) cn.ConexionBD.prepareStatement(query);
+     /*       parametro.setString(1, getNit());
+            parametro.setString(2, getNombres());
+            parametro.setString(3, getApellidos());
+            parametro.setString(4, getDireccion());
+            parametro.setString(5, getTelefono());
+            parametro.setString(6, getFecha_nacimiento());
+    */      parametro.setInt(1, getId());            
+            int executar = parametro.executeUpdate();
+            System.out.println("Borrado Exitoso... " + Integer.toString(executar));
+            
+            cn.cerrar_conexion( );
+            
+        
+        }catch(SQLException ex){
+                System.out.println("Error al Borrar" + ex.getMessage());
+        }
+}
+
+    
+//crud
+/*
+@Override
+protected String[] crear(){
+try{
+//codigo fuente
+String datos[]= new String[6];
+datos[0]=getNit();
+datos[1]=getNombres();
+datos[2]=getApellidos();
+datos[3]=getDireccion();
+datos[4]=getTelefono();
+datos[5]= this.getFecha_nacimiento();
+return datos;
+} catch (Exception ex){
+    System.out.println("Algo salido mal" + ex.getMessage());
+    return null;
+    }
+}
+*/
+    
+}
